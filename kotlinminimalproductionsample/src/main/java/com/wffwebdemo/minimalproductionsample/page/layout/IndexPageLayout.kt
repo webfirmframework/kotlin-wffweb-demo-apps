452b2683a6dd7f001e068b0c8e0eb83e1a55b2bb
Web Firm Framework <webfirm.framework@gmail.com>
2018-02-11 13:57:14 +0530

    Included prependDocType

--- kotlinminimalproductionsample/src/main/java/com/wffwebdemo/minimalproductionsample/page/layout/IndexPageLayout.kt
+++ kotlinminimalproductionsample/src/main/java/com/wffwebdemo/minimalproductionsample/page/layout/IndexPageLayout.kt
@@ -26,6 +26,7 @@ class IndexPageLayout(private val documentModel: DocumentModel) : Html(null), Se
 
     init {
         super.setSharedData(documentModel)
+        super.setPrependDocType(true)
         develop()
     }
 
